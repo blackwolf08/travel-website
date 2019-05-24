640e943336f19686e7db4c23daf0ddb329d4fc10
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-24 14:42:11 +0530

    JS lib init

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 Splitting();
+var scroll = new SmoothScroll('a[href*="#"]');
+let example = document.getElementById('example');
+
+inView('.example')
+    .on('enter', el => {
+        el.style.opacity = 1;
+        console.log("inview");
+    })
+    .on('exit', el => {
+        el.style.opacity = 0.5;
+        console.log("out view");
+    });
+
+{/* <a data-scroll href="#bazinga">Anchor Link</a>
+...
+<div id="bazinga">Bazinga!</div> */}
