b5f9616a6e534fcc04445f23eb808fae04067068
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-05-24 16:07:45 +0530

    iOS fixes: ::before used

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 Splitting();
 var scroll = new SmoothScroll('a[href*="#"]');
-let example = document.getElementById('example');
-
-inView('.example')
-    .on('enter', el => {
-        el.style.opacity = 1;
-        console.log("inview");
-    })
-    .on('exit', el => {
-        el.style.opacity = 0.5;
-        console.log("out view");
-    });
 
 {/* <a data-scroll href="#bazinga">Anchor Link</a>
 ...
